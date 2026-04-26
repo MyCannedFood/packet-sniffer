231dbc78f6abca4306b0fe522f93cc7dea9196c0
MyCannedFood <sagarawaluya@gmail.com>
2026-04-26 21:05:39 +0700

    add summary every 30 seconds

--- packetSniffer.py
+++ packetSniffer.py
@@ -13,6 +13,11 @@ sniffer_socket.bind((interface, 0))
 scan_tracker = defaultdict(list) 
 scan_tracked = defaultdict(set)
 
+ip_counter = defaultdict(int)
+port_counter = defaultdict(int)
+protocol_counter = defaultdict(int)
+last_summary = datetime.now()
+
 init_data = ["timestamp", 
              "src_IP", 
              "dst_IP", 
@@ -46,15 +51,14 @@ with open('log.csv', mode='w', newline='') as file:
                     src_port = packet[UDP].sport
                     dst_port = packet[UDP].dport
                     protocol = "UDP"
-
-                
+   
                 if packet.haslayer(IP):
                     src_ip = packet[IP].src
                     dst_ip = packet[IP].dst
     
                     scan_tracker[src_ip].append((dst_port, datetime.now()))
-
-                    print(scan_tracker)
+                    ip_counter[src_ip] += 1
+                    port_counter[dst_port] += 1
             
             writer.writerow([datetime.now(), 
                              src_ip, 
@@ -72,7 +76,22 @@ with open('log.csv', mode='w', newline='') as file:
 
                     if len(recent_ports) > 9:
                         print("alert!" + src_ip)
+
+            if src_ip:
+                ip_counter[src_ip] += 1 
             
+            if protocol:
+                protocol_counter[protocol] += 1
+           
+            if datetime.now() - last_summary > timedelta(seconds=30):
+                for src_ip in ip_counter:
+                    print(src_ip, ip_counter[src_ip])
+                for protocol in protocol_counter:
+                    print(protocol, protocol_counter[protocol])
+
+                ip_counter.clear()
+                protocol_counter.clear()
+                last_summary = datetime.now()
             # print(packet.summary())
     
     except KeyboardInterrupt:
